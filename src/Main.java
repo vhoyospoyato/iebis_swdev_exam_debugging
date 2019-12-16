2b3b6bebb9f8f3413d9fbf7c30a809e786e82b38
Vichy <vhoyos.ieu2017@student.ie.edu>
2019-12-16 12:29:20 +0100

    Replaced some parameters and added breaks in the switch for it to work

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,18 @@ public class Main {
         Random random = new Random();
         StringBuffer word = null;
 
-        switch (random.nextInt(2)) {
+        switch (random.nextInt(3)) {
             case 0:
                 word = new StringBuffer('Y');
+                break;
             case 1:
                 word = new StringBuffer('F');
+                break;
+
             case 2:
                 word = new StringBuffer('T');
+                break;
+
         }
 
         word.append('o');
